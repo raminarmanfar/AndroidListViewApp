3031662e7e2d3c7d3d1c0408c374b5faf74dc83f
raminarman <ramin.armanfar@msg.group>
2019-12-30 14:40:51 +0100

    first commi

---
+++ app/src/main/java/com/raminarman/listviewapp/MainActivity.java
@@ -0,0 +1,72 @@
+package com.raminarman.listviewapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    private Spinner spinner;
+    private ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.txtTitle);
+        textView.setText(getString(R.string.welcome_text));
+
+        spinner = findViewById(R.id.spinner);
+        listView = findViewById(R.id.listView);
+
+        final ArrayList<String> names = new ArrayList<>();
+        names.add("Ramin");
+        names.add("Roya");
+        names.add("Amin");
+
+        ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, names);
+        spinner.setAdapter(spinnerAdapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "You tapped on the spinner item " + names.get(position), Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                Toast.makeText(MainActivity.this, "Nothing has been selected on the spinner!", Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+        ArrayAdapter<String> listViewAdapter = new ArrayAdapter<>(this, android.R.layout.simple_expandable_list_item_1, names);
+        listView.setAdapter(listViewAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "You tapped on the item " + names.get(position), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "You have long tapped on the item " + names.get(position), Toast.LENGTH_LONG).show();
+                return false;
+            }
+        });
+    }
+}
