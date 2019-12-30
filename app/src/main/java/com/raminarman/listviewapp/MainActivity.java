39edf517243562f785e4f1165fbc6ab870b6a069
raminarman <ramin.armanfar@msg.group>
2019-12-30 23:31:44 +0100

    Tutorial done. 4

--- app/src/main/java/com/raminarman/listviewapp/MainActivity.java
+++ app/src/main/java/com/raminarman/listviewapp/MainActivity.java
@@ -51,6 +51,7 @@ public class MainActivity extends AppCompatActivity {
 
         textView = findViewById(R.id.txtTitle);
         textView.setText(getString(R.string.welcome_text));
+
         spinner = findViewById(R.id.spinner);
         listView = findViewById(R.id.listView);
 
